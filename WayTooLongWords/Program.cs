3593bae44e29bc2df5cb5cf26691951de05ff5ee
unknown <islammohamedahmed49@gmail.com>
2024-03-16 18:44:42 +0200

    way too long words

---
+++ WayTooLongWords/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+int n;
+n = ToInt32(ReadLine());
+string[] arr = new string[101];
+for (int i = 0; i < n; i++)
+{
+    string str = ReadLine();
+    if (str.Length <= 10)
+    {
+        arr[i] = str;
+    }
+    else
+    {
+        arr[i] = $"{str[0]}{str.Length - 2}{str[str.Length-1]}";
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    WriteLine(arr[i]);
+}
